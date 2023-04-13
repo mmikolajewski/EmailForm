fda221ead8abb9472931a3f00015fcb76f446677
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-04-13 12:06:38 +0100

    START

---
+++ src/main/java/pl/javastart/emailform/EmailFormApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.emailform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/emailform/EmailFormApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.emailform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
