c032edd6268e7df23e0528067beb45621254313b
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-04-14 13:09:43 +0100

    solution 2

--- src/main/java/pl/javastart/emailform/MailService.java
+++ src/main/java/pl/javastart/emailform/MailService.java
@@ -4,6 +4,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -12,22 +13,22 @@ import org.springframework.stereotype.Service;
 public class MailService {
 
     private final static String CONFIRMATION_MESSAGE_FOR_SENDER = "Dziękujemy za kontakt z SZOPPINGWORD, niedługo otrzymasz wiadomość";
-    private final static String OWNER_EMAIL_ADDRESS = "szopping@byom.de";
-
     private static final Logger logger = LoggerFactory.getLogger(MailService.class);
 
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
+    private final String ownerAddress;
 
-    public MailService(JavaMailSender mailSender) {
+    public MailService(JavaMailSender mailSender, @Value("${app.owneremail}") String ownerAddress) {
         this.javaMailSender = mailSender;
+        this.ownerAddress = ownerAddress;
     }
 
     public void sendConfirmationToSender(String senderEmail) {
-        sendMail(senderEmail, OWNER_EMAIL_ADDRESS, senderEmail, CONFIRMATION_MESSAGE_FOR_SENDER);
+        sendMail(senderEmail, ownerAddress, senderEmail, CONFIRMATION_MESSAGE_FOR_SENDER);
     }
 
     public void sendEmailToOwner(String senderEmail, String senderName,String text) {
-        sendMail(senderEmail, senderName, OWNER_EMAIL_ADDRESS, text);
+        sendMail(senderEmail, senderName, ownerAddress, text);
 
     }
 
@@ -36,9 +37,8 @@ public class MailService {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-            String htmlMsg = "<3>Hello World</h3>";
-            helper.setFrom(""); //email pośredniczący - ten sam co w .yml
-            helper.setTo(reciever);
+            helper.setFrom("szopping@byom.de"); //email pośredniczący - ten sam co w .yml
+            helper.setTo(ownerAddress);
             helper.setSubject(text);
 
             helper.setText("Od " + senderEmail + ", " + senderName + " " + text, true);
